0f2a9ba2609600eb8f0ab23ce0e8d68b0e6f394a
chrispdharman <christopher.p.d.harman@gmail.com>
2020-09-12 16:31:20 +0100

    Added player movement and player collider box

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    public Rigidbody2D playerRigidBody;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        playerRigidBody.MovePosition(playerRigidBody.position + movement * playerSpeed * Time.fixedDeltaTime);
+    }
+}
